ee0d8491a24ce174013c81a4e0857c2fc33a1390
shimaaFathi <shimaafathi126@gmail.com>
2024-03-04 02:18:14 +0200

    Run

---
+++ Day_one/Band_name.py
@@ -0,0 +1,5 @@
+
+print("Welcome to the Band Name Generator")
+name_of_city = input("What's the name of the city you grew up in?\n")
+bit_name = input("What's your bit name?\n")
+print("Your band name could be "+name_of_city+" "+bit_name)
